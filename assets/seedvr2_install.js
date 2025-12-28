d5787bf4d72352c326c66adf22deb0fc85184494
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-12-28 15:31:16 -0500

    Fix installable

--- assets/seedvr2_install.js
+++ assets/seedvr2_install.js
@@ -1,2 +1,5 @@
 // addInstallButton(groupId, featureId, installId, buttonText)
-addInstallButton('seedvr2upscaler', 'seedvr2_upscaler', 'seedvr2_upscaler', 'Install SeedVR2 Upscaler Node');
+// Place install button in "Refine / Upscale" group (groupId: refineupscale) since that's where
+// the upscale method dropdown lives. The SeedVR2 Upscaler param group won't render when
+// the feature is missing because all its params have FeatureFlag: "seedvr2_upscaler".
+addInstallButton('refineupscale', 'seedvr2_upscaler', 'seedvr2_upscaler', 'Install SeedVR2 Upscaler Node');
